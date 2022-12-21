d27919d259da001ff80c5c22b3708e5a3b27e562
erikesund <eriksund84@gmail.com>
2022-12-21 15:21:26 +0000

    hard coded data copied from a schedule step 1

--- cab-app/src/App.js
+++ cab-app/src/App.js
@@ -1,13 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-// import data from './data.json'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
 
+  const [newData, setNewData] = useState([]);
+  const [locationArray, setLocationArray] = useState([]);
+
+  
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      const response = await fetch("./test.json")
+      const data = await response.json()
+      setNewData(data)
+    }
+    try {
+    fetchdata()
+    } catch (error) {
+      console.error(error)
+    }
+  }, []) 
+
   return (
     <div className="App">
-      Hello
+      <p>Schedule Start Date: {newData.JsonScheduleV1.schedule_start_date}</p>
+      <p>Schedule End Date: {newData.JsonScheduleV1.schedule_end_date}</p>
+      <p>Departure: {newData.JsonScheduleV1.schedule_segment.schedule_location[0].departure} From: {newData.JsonScheduleV1.schedule_segment.schedule_location[0].tiploc_code}</p>
+      <p>Departure: {newData.JsonScheduleV1.schedule_segment.schedule_location[1].departure} From: {newData.JsonScheduleV1.schedule_segment.schedule_location[1].tiploc_code}</p>
+      <p>Departure: {newData.JsonScheduleV1.schedule_segment.schedule_location[2].departure} From: {newData.JsonScheduleV1.schedule_segment.schedule_location[2].tiploc_code}</p>
+      <p>Departure: {newData.JsonScheduleV1.schedule_segment.schedule_location[3].departure} From: {newData.JsonScheduleV1.schedule_segment.schedule_location[3].tiploc_code}</p>
+      <p>Departure: {newData.JsonScheduleV1.schedule_segment.schedule_location[4].departure} From: {newData.JsonScheduleV1.schedule_segment.schedule_location[4].tiploc_code}</p>
+      <p>Departure: {newData.JsonScheduleV1.schedule_segment.schedule_location[5].departure} From: {newData.JsonScheduleV1.schedule_segment.schedule_location[5].tiploc_code}</p>
+      <p>Departure: {newData.JsonScheduleV1.schedule_segment.schedule_location[6].departure} From: {newData.JsonScheduleV1.schedule_segment.schedule_location[6].tiploc_code}</p>
+      <p>Departure: {newData.JsonScheduleV1.schedule_segment.schedule_location[7].departure} From: {newData.JsonScheduleV1.schedule_segment.schedule_location[7].tiploc_code}</p>
+      <p>Departure: {newData.JsonScheduleV1.schedule_segment.schedule_location[8].departure} From: {newData.JsonScheduleV1.schedule_segment.schedule_location[8].tiploc_code}</p>
     </div>
   );
 }
