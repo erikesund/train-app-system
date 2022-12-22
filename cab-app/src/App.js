5065cc2b9b84f67064ec439ae7f332fe043a0096
erikesund <eriksund84@gmail.com>
2022-12-22 15:34:32 +0000

    movied driver state to driver container

--- cab-app/src/App.js
+++ cab-app/src/App.js
@@ -6,21 +6,6 @@ import DriverContainer from './containers/DriverContainer';
 
 function App() {
 
-  const [newData, setNewData] = useState([]);
-  const [locationArray, setLocationArray] = useState([]);
-
-  const getData = async () => {
-    const response = await fetch("./test.json")
-    const data = await response.json()
-    setNewData(data)
-    setLocationArray(data.JsonScheduleV1.schedule_segment.schedule_location)
-  }
-
-  useEffect(() => {
-    getData();
-  }, [])
-
-
   return (
     <div className="App">
       <DriverLogin/>
