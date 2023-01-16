9c60e3eff3ba31fb819feccb17398072d5d6e482
erikesund <eriksund84@gmail.com>
2023-01-16 13:12:16 +0000

    tidied up linereader

--- office-app/src/LineReader.js
+++ office-app/src/LineReader.js
@@ -1,26 +1,8 @@
 
-var dbName = 'schedule_feed';
-// var url = 'mongodb://localhost:27017/';
-var collectionName = 'all_data';
-// // var fileName = './toc-full'
-
-new function jsonReader() {
-    const readline = require('readline');
-    const fs = require('fs');
-  
-    const rl = readline.createInterface({
-      input: fs.createReadStream('./toc-full')
-    });
-    
-    rl.on('line', (line) => {
-      let parsedLine = JSON.parse(line)
-      console.log('Line from file:', parsedLine);
-      // dbName.collectionName.insertOne(parsedLine)
-    });
-  }
-
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://127.0.0.1:27017/";
+var dbName = 'schedule_feed';
+var collectionName = 'all_data';
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
@@ -34,24 +16,11 @@ MongoClient.connect(url, function(err, db) {
   
   rl.on('line', (line) => {
     let parsedLine = JSON.parse(line)
-    // console.log('Line from file:', parsedLine);
-    // dbName.collectionName.insertOne(parsedLine)
     dbo.collection(collectionName).insertOne(parsedLine, function(err, res) {
       if (err) throw err;
   });
-  console.log("Documents inserted");
   // db.close();
 });
 });
 
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   var dbo = db.db(dbName);
-//   var myobj = { name: "Company Inc", address: "Highway 37" };
-//   dbo.collection(collectionName).insertOne(myobj, function(err, res) {
-//     if (err) throw err;
-//     console.log("Documents inserted");
-//     db.close();
-//   });
-// });
 
