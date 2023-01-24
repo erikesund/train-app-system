63d54b89eb942f9ae0c8bb2e9805f16bc4d9e37d
erikesund <eriksund84@gmail.com>
2023-01-24 16:36:41 +0000

    express setup

--- office-app/db/helpers/db.js
+++ office-app/db/helpers/db.js
@@ -3,14 +3,16 @@ const { MongoClient } = require("mongodb")
 let dbConnection;
 
 module.exports = {
-  connectToDb: () => {
+  connectToDb: (callback) => {
     MongoClient.connect("mongodb://127.0.0.1:27017/schedule_feed")
       .then((client) => {
         dbConnection = client.db()
+        return callback()
       })
       .catch(err => {
         console.log(err)
+        return callback(err)
       })
   },
-  getDb: () => {}
+  getDb: () => dbConnection
 }
