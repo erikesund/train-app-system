a068097094c672cb5c49acd62aee6ac2a12c497d
erikesund <eriksund84@gmail.com>
2023-01-26 13:32:43 +0000

    get all working, routes all set

--- office-app/src/db/server.js
+++ office-app/src/db/server.js
@@ -2,8 +2,6 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 app.use(cors())
-
-
 app.use(express.json());
 const MongoClient = require("mongodb").MongoClient;
 const createRouter = require("./create_router")
@@ -12,36 +10,17 @@ MongoClient.connect('mongodb://127.0.0.1:27017')
   .then((client) => {
     const db = client.db("schedule_feed")
     const tiplocsCollection = db.collection("tiplocs")
+    const associationsCollection = db.collection("associations")
+    const schedulesCollection = db.collection("schedules")
     const tiplocsRouter = createRouter(tiplocsCollection)
+    const associationsRouter = createRouter(associationsCollection)
+    const schedulesRouter = createRouter(schedulesCollection)
 
-    app.use("/schedule_feed/tiplocs", tiplocsRouter)
+    app.use("/tiplocs", tiplocsRouter)
+    app.use("/associations", associationsRouter)
+    app.use("/schedules", schedulesRouter)
   })
 
   app.listen(9000, function() {
     console.log("App running on port 9000")
   })
-
-// let db;
-
-// connectToDb((err) => {
-//   if(!err) {
-//     app.listen(3000, () => {
-//       console.log("app listening on port 3000")
-//     })
-//     db = getDb()
-//   }
-// })
-
-// app.get("/schedule_feed", (req, res) => {
-//   let tiplocs = []
-//   db.collection('tiplocs')
-//     .find() //cursor toArray forEach
-//     //sort by name
-//     .forEach(tiploc => tiplocs.push(tiploc))
-//     .then(() => {
-//       res.status(200).json(tiplocs)
-//     })
-//     .catch(() => {
-//       res.status(500).json({error: "Could not fetch objects"})
-//     })
-// })
