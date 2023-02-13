720abd0bbc8ac666569defd53f026c95d414d348
erikesund <eriksund84@gmail.com>
2023-02-13 12:24:48 +0000

    added driver route and collection to db

--- office-app/src/db/server.js
+++ office-app/src/db/server.js
@@ -12,13 +12,16 @@ MongoClient.connect('mongodb://127.0.0.1:27017')
     const tiplocsCollection = db.collection("tiplocs")
     const associationsCollection = db.collection("associations")
     const schedulesCollection = db.collection("schedules")
+    const driversCollection = db.collection("drivers")
     const tiplocsRouter = createRouter(tiplocsCollection)
     const associationsRouter = createRouter(associationsCollection)
     const schedulesRouter = createRouter(schedulesCollection)
+    const driversRouter = createRouter(driversCollection)
 
     app.use("/tiplocs", tiplocsRouter)
     app.use("/associations", associationsRouter)
     app.use("/schedules", schedulesRouter)
+    app.use("/drivers", driversRouter)
   })
 
   app.listen(9000, function() {
